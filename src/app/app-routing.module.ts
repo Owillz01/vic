b66edd2ef6ebc5ffaf09017de0a3ea479577cfd3
owillz01 <omonkhodion@gmail.com>
2020-01-15 17:32:19 +0100

    done with most of the structure of the home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { AboutComponent } from './pages/about/about.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component:HomeComponent},
+  {path: "contact", component:ContactComponent},
+  {path: "service", component:ServicesComponent},
+  {path: "blog", component:BlogComponent},
+  {path: "about", component:AboutComponent},
+  {path: "**", component:ErrorComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,37 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './core/header/header.component';
+import { FooterComponent } from './core/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { ServicesComponent } from './pages/services/services.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { ErrorComponent } from './pages/error/error.component';
+import { AboutComponent } from './pages/about/about.component';
+
+import { StickyNavModule } from 'ng2-sticky-nav';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    BlogComponent,
+    ServicesComponent,
+    ContactComponent,
+    ErrorComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    StickyNavModule 
   ],
   providers: [],
   bootstrap: [AppComponent]
